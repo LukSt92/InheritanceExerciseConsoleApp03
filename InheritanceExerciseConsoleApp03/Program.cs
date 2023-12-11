c061d471897dedad39d813367da89fceb3a5e4ab
LukSt92 <lukasz.m.stasiak@gmail.com>
2023-12-11 12:30:03 +0100

    added data.

--- InheritanceExerciseConsoleApp03/Program.cs
+++ InheritanceExerciseConsoleApp03/Program.cs
@@ -6,10 +6,15 @@ using System.Threading.Tasks;
 
 namespace InheritanceExerciseConsoleApp03
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            SafeOwner owner = new SafeOwner();
+            Safe safe = new Safe();
+            JewelThief jewelThief = new JewelThief();
+            jewelThief.OpenSafe(safe, owner);
+            Console.ReadKey(true);
         }
     }
 }
